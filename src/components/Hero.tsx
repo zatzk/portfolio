67fb77556185e5abd8f789708ddb8ac96eeeefe4
Jean Luiz <zatzk@outlook.com>
2022-08-14 11:02:27 -0300

    changed fontSize and Style, added delay

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,30 +1,58 @@
-import { Container, Flex, Heading } from '@chakra-ui/react'
+import { Box, Container, Flex, Heading } from '@chakra-ui/react'
+import React, { useEffect } from "react";
+import { useState } from "react";
+import WavyText from "./WavyText";
 
-export const Hero = ({ title }: { title: string }) => (
-  <Container 
-  maxW="container.lg"
-  padding="50px"
-  height="50%"
-  flexWrap="wrap"
-  alignContent="center"
-  >
-    <Flex
-    justifyContent="left"
-    alignItems="center"
-    bgGradient="linear(to-l, heroGradientStart, heroGradientEnd)"
-    bgClip="text"
-    
-  >
-    <Heading 
-      fontWeight="700" 
-      fontStyle="normal" 
-      fontSize="90"
-      >{title}</Heading>
-  </Flex>
-  </Container>
+
+export function Hero() {
+  const [replay, setReplay] = useState(true);
+  const [showComponent, setShowComponent] = useState(false);
+
+  useEffect(() => {
+    setInterval(() => {
+      setShowComponent(!showComponent);
+    }, 1000);
+  }, []);
   
-)
+  return(
+    <Container 
+    maxW="container.lg"
+    padding="50px"
+    height="100%"
+    flexWrap="wrap"
+    alignContent="center"
+    >
+      <Flex
+      justifyContent="left"
+      alignItems="center"
+      bgGradient="linear(to-l, heroGradientStart, heroGradientEnd)"
+      bgClip="text"
+      
+    >
+      <Box 
+        fontWeight="bold"
+        fontStyle="normal" 
+        fontSize={{base: 50, md: 80, lg: 90}}
+        >
+          <WavyText text="hi, im zatzk" replay={replay}/>
+        </Box>
+      </Flex>
 
-Hero.defaultProps = {
-  title: 'hi, im zatzk',
+
+    
+      <Flex> 
+        <Box
+          fontWeight="bold" 
+          fontStyle="normal" 
+          fontSize={{base: 15, md: 20, lg: 30}}
+        >
+          {showComponent && <WavyText text="Developer" replay={replay}/>}
+          
+       </Box>
+      </Flex>
+    </Container>
+    
+  )
 }
+
+
