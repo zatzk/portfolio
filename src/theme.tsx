07d66f3ba5d2a28ec8f6c0364d2618c2abf30f38
Jean Luiz <zatzk@outlook.com>
2022-12-16 16:32:06 -0300

    added jetbrains mono, tested new hover color

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,9 +1,11 @@
 import { extendTheme } from '@chakra-ui/react'
 import "@fontsource/karla"
 import "@fontsource/roboto"
+import "@fontsource/jetbrains-mono"
 
-const fonts = { primary: 'karla', 
-                secondary: 'roboto bold' }
+const fonts = { primary: 'Karla', 
+                secondary: 'JetBrains Mono'
+              }
 
 
 const theme = extendTheme({
@@ -29,13 +31,20 @@ const theme = extendTheme({
         default: 'gray.200',
         _dark: '#292929',
       },
+      hoverColor: {
+        default: '#0F5',
+          _dark: '#04E0D9',
+      },
     },
+    // boxShadow: {
+    //   shBox: {
+    //     default: '0 0 5px #D941B6, 0 0 25px #D941B6, 0 0 50px #D941B6, 0 0 200px #D941B6',
+    //     _dark: '0 0 5px #03e9f4, 0 0 25px #03e9f4, 0 0 50px #03e9f4, 0 0 200px #03e9f4',
+    //   },
+    // },
     radii: {
       button: '12px',
     },
-  },
-  colors: {
-    black: '#16161D',
   },
   fonts,
 })
