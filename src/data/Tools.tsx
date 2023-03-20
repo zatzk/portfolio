46c77d195a2224a435c864b0e2bb6bb4f8e73287
zatzk <zatzk@outlook.com>
2023-03-20 12:04:48 -0300

    feat: added styled-icons and updated tools list

---
+++ src/data/Tools.tsx
@@ -0,0 +1,159 @@
+import { Astro } from "@styled-icons/simple-icons/Astro";
+import { Leaflet } from "@styled-icons/simple-icons/Leaflet";
+import { Tailwindcss } from "@styled-icons/simple-icons/Tailwindcss";
+import { Json } from "@styled-icons/simple-icons/Json";
+import { Heroku } from "@styled-icons/simple-icons/Heroku";
+import { Framer } from "@styled-icons/simple-icons/Framer";
+import { Html5 } from "@styled-icons/simple-icons/Html5";
+import { Css3 } from "@styled-icons/simple-icons/Css3";
+import { Javascript } from "@styled-icons/simple-icons/Javascript";
+import { Typescript } from "@styled-icons/simple-icons/Typescript";
+import { ReactLogo } from "@styled-icons/simple-icons/ReactLogo";
+import { Nodedotjs } from "@styled-icons/simple-icons/Nodedotjs";
+import { Nextdotjs } from "@styled-icons/simple-icons/Nextdotjs";
+import { Electron } from "@styled-icons/simple-icons/Electron";
+import { Figma } from "@styled-icons/simple-icons/Figma";
+import { Chakraui } from "@styled-icons/simple-icons/Chakraui";
+import { Storybook } from "@styled-icons/simple-icons/Storybook";
+import { Adobe } from "@styled-icons/simple-icons/Adobe";
+import { Socketdotio } from "@styled-icons/simple-icons/Socketdotio";
+import { Sass } from "@styled-icons/simple-icons/Sass";
+
+import { v4 as uuidv4 } from 'uuid';
+
+
+export const astro = {
+  name: 'astro',
+  icon: Astro,
+  color: '#FF0000',
+  id: uuidv4()
+};
+export const leaflet = {
+  name: 'leaflet',
+  icon: Leaflet,
+  color: '#0fd620',
+  id: uuidv4()
+};
+export const tailwind = {
+  name: 'tailwind',
+  icon: Tailwindcss,
+  color: '#0FB6D6',
+  id: uuidv4()
+};
+export const json = {
+  name: 'json',
+  icon: Json,
+  color: '#808080',
+  id: uuidv4()
+};
+export const heroku = {
+  name: 'Heroku',
+  icon: Heroku,
+  color: '#800080',
+  id: uuidv4()
+};
+export const framer = {
+  name: 'Framer Motion',
+  icon: Framer,
+  color: '#800080',
+  id: uuidv4()
+};
+
+export const html5 = {
+  name: 'HTML5',
+  icon: Html5,
+  color: '#E34F26',
+  id: uuidv4()
+};
+
+export const css3 = {
+  name: 'CSS3',
+  icon: Css3,
+  color: '#1572B6',
+  id: uuidv4()
+};
+
+export const js = {
+  name: 'JavaScript',
+  icon: Javascript,
+  color: '#F7DF1E',
+  id: uuidv4()
+};
+
+export const typescript = {
+  name: 'Typescript',
+  icon: Typescript,
+  color: '#3178C6',
+  id: uuidv4()
+};
+
+export const react = {
+  name: 'React',
+  icon: ReactLogo,
+  color: '#61DAFB',
+  id: uuidv4()
+};
+
+export const nodejs = {
+  name: 'Node.js',
+  icon: Nodedotjs,
+  color: '#339933',
+  id: uuidv4()
+};
+
+export const nextjs = {
+  name: 'Next.js',
+  icon: Nextdotjs,
+  color: '#a3a3a3',
+  id: uuidv4()
+};
+
+export const electron = {
+  name: 'Electron',
+  icon: Electron,
+  color: '#47848F',
+  id: uuidv4()
+};
+
+export const figma = {
+  name: 'Figma',
+  icon: Figma,
+  color: '#F24E1E',
+  id: uuidv4()
+};
+
+// add chakraui svg to files and import
+export const chakra = {
+  name: 'Chakra UI',
+  icon: Chakraui,
+  color: '#319795',
+  id: uuidv4()
+};
+
+export const storybook = {
+  name: 'Storybook',
+  icon: Storybook,
+  color: '#FF4785',
+  id: uuidv4()
+};
+
+export const adobe = {
+  name: 'Adobe',
+  icon: Adobe,
+  color: '#FF0000',
+  id: uuidv4()
+};
+
+export const socketIO = {
+  name: 'Socket IO',
+  icon: Socketdotio,
+  color: null,
+  id: uuidv4()
+};
+
+export const sass = {
+  name: 'Sass',
+  icon: Sass,
+  color: '#CC6699',
+  id: uuidv4()
+};
