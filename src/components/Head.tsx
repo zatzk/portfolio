ffaa5e2f2280c43fc1e841d81913e5d12285563f
zatzk <zatzk@outlook.com>
2023-03-20 12:09:24 -0300

    chore: added dynamic Head and interfaces

---
+++ src/components/Head.tsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+
+export interface HeadProps {
+  title: string;
+  name: string;
+}
+
+export function HeadTag({title, name}: HeadProps) {
+  return (
+    
+    <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content={name}/>
+        <meta name="author" content="Jean Luiz" />
+        <meta name="author" content="zatzk" />
+        <link rel="icon" href="favicon.svg" type="image/svg+xml" />
+        <meta property="og:site_name" content="zatzk's portfolio" />
+        <meta name="og:title" content="zatzk's portfolio" />
+        <meta property="og:type" content="website" />
+        <title>zatzk&#39;s portfolio - {title}</title>
+      </Head>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,35 +3,34 @@ import { Main } from '../components/Main'
 import { Footer } from '../components/Footer'
 import { Navbar } from '../components/Navbar'
 import { Hero } from '../components/Hero'
-import Head from 'next/head'
 import { Projects } from '../components/Projects'
 import { SocialsBar } from '../components/SocialsBar'
+import { HeadTag, HeadProps } from '../components/Head'
 
-const Index = () => (
-  <Container height="100%">
-    <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="zatzk's homepage" />
-        <meta name="author" content="Jean Luiz" />
-        <meta name="author" content="zatzk" />
-        <link rel="icon" href="favicon.svg" type="image/svg+xml" />
-        <meta property="og:site_name" content="zatzk's portfolio" />
-        <meta name="og:title" content="zatzk's portfolio" />
-        <meta property="og:type" content="website" />
-        <title>zatzk&#39;s portfolio - Projects</title>
-      </Head>
+interface IndexProps {
+  pageTitle: string;
+  pageName: string;
+}
 
-    <Navbar />
-    <SocialsBar />
-    <Container pt={14}>
-      <Main >
-        <Hero/>
-        <Projects />
-      </Main>
+export default function Index({ pageTitle, pageName }: IndexProps) {
+  pageTitle = 'Projects';
+  pageName = 'Homepage';
 
-      <Footer />
+  return (
+    <Container height="100%">
+      <HeadTag title={pageTitle} name={pageName}/>
+      
+      <Navbar />
+      <SocialsBar />
+      <Container pt={14}>
+        <Main >
+          <Hero/>
+          <Projects />
+        </Main>
+
+        <Footer />
+      </Container>
     </Container>
-  </Container>
-)
+  )
+} 
 
-export default Index
