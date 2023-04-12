d9f6be393088b654a3a0130b7047f9ce3e5f60e5
zatzk <zatzk@outlook.com>
2023-04-11 21:23:17 -0300

    feat: added new bg

--- src/components/designPage/ContainerDesign.tsx
+++ src/components/designPage/ContainerDesign.tsx
@@ -1,18 +1,38 @@
-import { Box, BoxProps } from '@chakra-ui/react'
+import React, { useState, useEffect } from 'react';
+import { Box, BoxProps } from '@chakra-ui/react';
 
-export const ContainerDesign = (props: BoxProps) => (
-  <Box
-    h="100dvh"
-    direction="column"
-    alignItems="center"
-    justifyContent="flex-start"
-    bgImage="/images/giphy.gif"
-    bgSize="cover"
-    bgPosition="center"
-    bgAttachment="fixed"
-    color="#d5ded7"
-    
-    transition="all 0.15s ease-out"
-    {...props}
-  />
-)
+export const ContainerDesign = (props: BoxProps) => {
+  const [opacity, setOpacity] = useState(0.6); // Initial opacity value
+
+  useEffect(() => {
+    const handleScroll = (event: WheelEvent) => {
+      
+      const delta = event.deltaY > 0 ? -0.1 : 0.1;
+      const newOpacity = Math.max(0, Math.min(opacity + delta, 0.6));
+      setOpacity(newOpacity);
+    };
+
+    window.addEventListener('wheel', handleScroll, { passive: false }); // Add wheel event listener
+
+    return () => {
+      window.removeEventListener('wheel', handleScroll); // Remove wheel event listener on component unmount
+    };
+  }, [opacity]);
+
+  return (
+    <Box
+      h="100vh"
+      direction="column"
+      alignItems="center"
+      justifyContent="flex-start"
+      bgImage="/images/untitled.gif"
+      bgSize="cover"
+      bgPosition="center"
+      bgAttachment="fixed"
+      color="#d5ded7"
+      style={{ filter: `opacity(${opacity})` }} // Set opacity based on state value
+      transition="all 0.15s ease-out"
+      {...props}
+    />
+  );
+};
