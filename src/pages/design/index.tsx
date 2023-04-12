0c781710a8113771f4c0dd4792afc4ca7ba5d520
zatzk <zatzk@outlook.com>
2023-04-11 21:22:58 -0300

    fix: changed container

--- src/pages/design/index.tsx
+++ src/pages/design/index.tsx
@@ -8,32 +8,32 @@ import { SocialsBar } from '../../components/SocialsBar'
 import { HeadTag } from '../../components/Head'
 import { HeadProps } from '../../interfaces/HeadProps'
 import { Box } from '@chakra-ui/react'
+import { Container } from '../../components/Container'
 
 export default function Design({ title, content }: HeadProps) {
   title = 'Designer Page';
   content = 'Designs';
 
   return (
-    <ContainerDesign height="100%">
+    <Container height="100%" bgColor="#141416">
       <HeadTag title={title} content={content}/>
       
       <NavbarDesign />
       {/* <SocialsBar /> */}
       <ContainerDesign pt={14}>
         <Main >
-          TEXTO DE APRESENTAÇÃO
+          TEXTO TEXTO TEXTO
           {/* <Hero/>
           <Projects /> */}
         </Main>
       </ContainerDesign>
       <Box
         h="100dvh"
-        bgColor="#141416"
       >
         aaa
       <Footer />
       </Box>
-    </ContainerDesign>
+    </Container>
   )
 } 
 
