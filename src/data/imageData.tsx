913fa20e7f2bbeb92ad0ecee54f97d469b54ed2f
zatzk <zatzk@outlook.com>
2023-04-28 14:41:14 -0300

    chore: add local temp imageData

---
+++ src/data/imageData.tsx
@@ -0,0 +1,44 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export default [
+  {
+    id: uuidv4(),
+    src: '/images/images_API/1.jpg',
+    thumb: '/images/images_API/thumb/th1.jpg'
+  },
+  {
+    id: uuidv4(),
+    src: '/images/images_API/2.jpg',
+    thumb: '/images/images_API/thumb/th2.jpg'
+  },
+  {
+    id: uuidv4(),
+    src: '/images/images_API/3.jpg',
+    thumb: '/images/images_API/thumb/th3.jpg'
+  },
+  {
+    id: uuidv4(),
+    src: '/images/images_API/4.jpg',
+    thumb: '/images/images_API/thumb/th4.jpg'
+  },
+  {
+    id: uuidv4(),
+    src: '/images/images_API/5.jpg',
+    thumb: '/images/images_API/thumb/th5.jpg'
+  },
+  {
+    id: uuidv4(),
+    src: '/images/images_API/6.jpg',
+    thumb: '/images/images_API/thumb/th6.jpg'
+  },
+  {
+    id: uuidv4(),
+    src: '/images/images_API/7.jpg',
+    thumb: '/images/images_API/thumb/th7.jpg'
+  },
+  {
+    id: uuidv4(),
+    src: '/images/images_API/8.jpg',
+    thumb: '/images/images_API/thumb/th8.jpg'
+  },
+]
