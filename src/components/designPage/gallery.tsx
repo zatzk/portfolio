b047c17e9e1d747244d428cd77423c5a09abc765
zatzk <zatzk@outlook.com>
2023-04-28 14:42:34 -0300

    feat: gallery component

---
+++ src/components/designPage/gallery.tsx
@@ -0,0 +1,38 @@
+import { Box, Container, SimpleGrid } from '@chakra-ui/react';
+import { useState } from 'react';
+import Image from 'next/image'
+
+import imageData from '../../data/imageData';
+
+
+export const Gallery = () => {
+ 
+
+
+  return (
+    <Container
+      maxW="85%"
+      paddingY="3.125rem"
+      height="100%"
+      flexWrap="wrap"
+      alignContent="center"
+      mr={0}
+    >
+
+      <SimpleGrid
+        // marginTop={10}
+        flexWrap="wrap"
+        display="grid"
+        minChildWidth="30rem"
+        columns={[1, 2, 3]}
+        gap="0.8rem"
+        padding="0px"
+          >
+        {imageData.map((image, index) => (
+          <Image height={5000} width={5000} key={index} src={image.src} alt="test" />
+          ))}
+        
+      </SimpleGrid>
+    </Container>
+  );
+};
