d196fa480d5a881bea79160edad3b5579063b8e6
zatzk <zatzk@outlook.com>
2023-09-17 21:51:46 -0300

    chore: removed unused code

--- src/pages/design/index.tsx
+++ src/pages/design/index.tsx
@@ -1,13 +1,9 @@
-import { ContainerDesign } from '../../components/designPage/ContainerDesign'
 import { FooterDesign } from '../../components/designPage/FooterDesign'
 import { NavbarDesign } from '../../components/designPage/NavbarDesign'
 import { HeadTag } from '../../components/Head'
 import { HeadProps } from '../../interfaces/HeadProps'
-import { Box } from '@chakra-ui/react'
 import { Container } from '../../components/Container'
-import { IntroDesign } from '../../components/designPage/IntroDesign'
 import { Gallery } from '../../components/designPage/Gallery'
-import { Main } from '../../components/Main'
 import { DesignProjects } from '../../components/designPage/DesignProjects'
 import { useState } from 'react'
 
@@ -22,21 +18,12 @@ export default function Design({ title, content }: HeadProps) {
   return (
     <Container minH="100dvh" className='secondThemeSwitch'>
       <HeadTag title={title} content={content}/>
-      
-      {/* <IntroDesign/> */}
-      {/* <ContainerDesign pt={20}/> */}
-      
+    
       <NavbarDesign/>
       <DesignProjects selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
       <Gallery selectedCategory={selectedCategory} /> 
-      
-
-      {/* <Box
-        // display={{base:"flex", md:"block"}}
-
-        >
-      </Box> */}
       <FooterDesign />
+
     </Container>
   )
 } 
