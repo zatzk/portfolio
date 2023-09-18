618df4ec4d8e02b71263de6c521bfbaa44cdff5b
zatzk <zatzk@outlook.com>
2023-09-17 21:51:29 -0300

    chore: about as his own index

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,42 @@
+import { Container } from '../../components/Container'
+import { Footer } from '../../components/Footer'
+import { Navbar } from '../../components/Navbar'
+import { SocialsBar } from '../../components/SocialsBar'
+import { AboutText } from '../../components/AboutText'
+import { HeadTag } from '../../components/Head'
+import { HeadProps } from '../../interfaces/HeadProps'
+import { IntroAbout } from '../../components/aboutPage/IntroAbout'
+import { ContainerAbout } from '../../components/aboutPage/ContainerAbout'
+import { AboutNavbar } from '../../components/aboutPage/AboutNavbar'
+
+
+export default function About({ title, content }: HeadProps) {
+  title = 'About me';
+  content = 'About me';
+  
+  return (
+    <Container 
+      minH="100dvh" className='thirdThemeSwitch'>
+      <HeadTag title={title} content={content}/>
+      
+      <AboutNavbar />
+      {/* <SocialsBar /> */}
+      <IntroAbout/>
+      <ContainerAbout/>
+      <AboutText /> 
+
+      <Footer />
+
+      {/* <Container pt={14}>
+        <Main >
+             <Btn  >
+              <Box as="a" textDecoration="none" href = "mailto: zatzk.cnt@outlook.com" target="_blank">
+                GET IN TOUCH
+              </Box>
+            </Btn> 
+            </Main> 
+      </Container> */}
+      
+    </Container>
+  )
+}
