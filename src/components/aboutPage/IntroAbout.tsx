d8afe12e980483a10322c8f219afb5a886517c6f
zatzk <zatzk@outlook.com>
2023-09-22 22:53:35 -0300

    style: update intro

--- src/components/aboutPage/IntroAbout.tsx
+++ src/components/aboutPage/IntroAbout.tsx
@@ -1,5 +1,6 @@
-import { BoxProps, Container, Flex, Text } from "@chakra-ui/react"
+import { BoxProps, Container, Flex, Text, keyframes } from "@chakra-ui/react"
 import { motion } from "framer-motion";
+import { css } from '@emotion/react'
 
 const line1 = [
   'SOFTWARE',
@@ -18,6 +19,66 @@ const line4 = [
 ]
 
 export function IntroAbout(props: BoxProps) {
+
+  const glitchAnimation = css`
+
+    .glitch {
+      text-shadow: 0.05em 0 0 #00fffc, -0.03em -0.04em 0 #fc00ff, 0.025em 0.04em 0 #fffc00;
+      animation: glitch 425ms infinite;
+      opacity: 0.75;
+    }
+
+    .glitch span {
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+
+    .glitch span:first-child {
+      animation: glitch 500ms infinite;
+      clip-path: polygon(0 0, 100% 0, 100% 35%, 0 35%);
+      transform: translate(-0.04em, -0.03em);
+      opacity: 0.75;
+    }
+
+    .glitch span:last-child {
+      animation: glitch 375ms infinite;
+      clip-path: polygon(0 65%, 100% 65%, 100% 100%, 0 100%);
+      transform: translate(0.04em, 0.03em);
+      opacity: 0.75;
+    }
+
+    @keyframes glitch {
+      0% {
+        text-shadow: 0.05em 0 0 #00fffc, -0.03em -0.04em 0 #fc00ff,
+          0.025em 0.04em 0 #fffc00;
+      }
+      15% {
+        text-shadow: 0.05em 0 0 #00fffc, -0.03em -0.04em 0 #fc00ff,
+          0.025em 0.04em 0 #fffc00;
+      }
+      16% {
+        text-shadow: -0.05em -0.025em 0 #00fffc, 0.025em 0.035em 0 #fc00ff,
+          -0.05em -0.05em 0 #fffc00;
+      }
+      49% {
+        text-shadow: -0.05em -0.025em 0 #00fffc, 0.025em 0.035em 0 #fc00ff,
+          -0.05em -0.05em 0 #fffc00;
+      }
+      50% {
+        text-shadow: 0.05em 0.035em 0 #00fffc, 0.03em 0 0 #fc00ff,
+          0 -0.04em 0 #fffc00;
+      }
+      99% {
+        text-shadow: 0.05em 0.035em 0 #00fffc, 0.03em 0 0 #fc00ff,
+          0 -0.04em 0 #fffc00;
+      }
+      100% {
+        text-shadow: -0.05em 0 0 #00fffc, -0.025em -0.04em 0 #fc00ff,
+          -0.04em -0.025em 0 #fffc00;
+      }
+    }
+  `
 
   const aboutText = {
     hidden: {
@@ -47,18 +108,32 @@ export function IntroAbout(props: BoxProps) {
         display="flex"
         alignItems="center"
         justifyContent="center"
+        css={glitchAnimation}
         // py={{base: "100px", md: "150px", lg:"300px"}}
         // px={{base: "100px", md: "150px", lg:"300px"}}
       >
-        <Flex fontSize="30px" fontWeight="900" fontFamily="secondary" flexDirection="column">
+        <Flex
+         zIndex="1"
+         fontSize="30px" 
+         fontWeight="900" 
+         fontFamily="secondary" 
+         flexDirection="column"
+         className="glitch"
+         cursor="default"
+         >
         <motion.div
             variants={aboutText} 
             initial="hidden"
             animate="show" 
             transition={{delay: 0.3, duration: 0.8}}
+            
           >
             {line1.map((text, index) => ( 
-              <Text key={index} whiteSpace="nowrap" display="inline-block">
+              <Text
+               key={index} 
+               whiteSpace="nowrap" 
+               display="inline-block"
+               >
                 {text}&nbsp;
               </Text>
             ))}
