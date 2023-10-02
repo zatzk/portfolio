dc33f643a0b821a94f486e50afa4fb515cddedf4
zatzk <zatzk@outlook.com>
2023-10-01 22:29:46 -0300

    feat: add album

--- src/components/designPage/gallery.tsx
+++ src/components/designPage/gallery.tsx
@@ -1,31 +1,94 @@
 /* eslint-disable react/prop-types */
 import { Container } from '@chakra-ui/react';
 import Image from 'next/image';
-import { imageData } from '../../data/imageData';
+import { SimpleGrid } from '@chakra-ui/react';
+import PhotoAlbum from "react-photo-album";
+import Lightbox from "yet-another-react-lightbox";
+import { useState } from "react";
 
-export const Gallery = ({ selectedCategory }: { selectedCategory: string }) => {
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+import "yet-another-react-lightbox/styles.css";
+
+
+export const Gallery = ({ selectedCategory, imgFetched }: { selectedCategory: string, imgFetched: any }) => {
+  const [index, setIndex] = useState(-1);
+  
+
   const getImageDataByCategory = () => {
     switch (selectedCategory) {
       case 'posters':
-        return imageData.poster;
+        return imgFetched.filter((image: any) => image.type === 'poster');
       case 'ui/ux':
-        return imageData.uiUx;
+        return imgFetched.filter((image: any) => image.type === 'ui/ux');
       case 'logos':
-        return imageData.logos;
+        return imgFetched.filter((image: any) => image.type === 'logos');
       default:
         return [];
     }
-  };
-
+  };  
   const images = getImageDataByCategory();
+
+  
+  const getSlides = images.map((image: any) => {
+    const width = 1080;
+    const height = 1350;
+    return {
+      src: image.imageSrc,
+      width,
+      height
+    }
+  });
+
 
   return (
     <Container minH="100dvh" position="relative" maxW="100%" paddingLeft={{base: '20%', md:'10%', lg: '8%', xl: '6%'}} paddingTop="8rem">
-      <div>
-        {images.map((image) => (
-          <Image key={image.id} src={image.src} width={90} height={90} alt="test" />
+      <PhotoAlbum
+       photos={getSlides} 
+       layout="rows" 
+       targetRowHeight={500} 
+       onClick={({ index }) => setIndex(index)} 
+       />
+      <Lightbox
+        slides={getSlides}
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+        // enable optional lightbox plugins
+        // plugins={[Slideshow, Zoom]}
+      />
+      
+      {/* <SimpleGrid
+        flexWrap="wrap"
+        display="grid"
+        minChildWidth="18.5em"
+        columns={[1, 2, 3]}
+        gap="1rem"
+        padding="0px"
+      >
+      {images.map((image) => (
+        <Image 
+          key={image.id} 
+          src={image.imageSrc} 
+          width={500} 
+          height={500} 
+          alt="test" 
+          onClick={(({index: current}) => setIndex(image.id))}
+          />
         ))}
-      </div>
+        <Lightbox
+          index={index}
+          position="absolute"
+          slides={getSlides}
+          open={index >= 0}
+          close={() => setIndex(-1)}
+        />
+      </SimpleGrid>
+     */}
     </Container>
   );
 };
+
