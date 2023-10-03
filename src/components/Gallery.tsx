133e378cf626afb40146602bf53e735a2cf2bcce
zatzk <zatzk@outlook.com>
2023-10-02 22:02:43 -0300

    chore: add loader

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
-import { Container } from '@chakra-ui/react';
+import { Container, Spinner } from '@chakra-ui/react';
 import PhotoAlbum from "react-photo-album";
 import Lightbox from "yet-another-react-lightbox";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 import "yet-another-react-lightbox/styles.css";
@@ -10,8 +10,35 @@ import "yet-another-react-lightbox/styles.css";
 
 export const Gallery = ({ selectedCategory, imgFetched }: { selectedCategory: string, imgFetched: any }) => {
   const [index, setIndex] = useState(-1);
-  
+  const [imagesLoaded, setImagesLoaded] = useState(false); // Track if all images are loaded
 
+  useEffect(() => {
+    const imageElements = document.querySelectorAll('.gallery-image');
+
+    const handleImageLoad = () => {
+      const loadedImages = Array.from(imageElements).filter((image) => {
+        return (image as HTMLImageElement).complete;
+      });
+
+      if (loadedImages.length === imageElements.length) {
+        setImagesLoaded(true); // All images are loaded
+      }
+    };
+
+    handleImageLoad(); // Check initial load state
+
+    imageElements.forEach((image) => {
+      image.addEventListener('load', handleImageLoad);
+    });
+
+    return () => {
+      // Clean up event listeners
+      imageElements.forEach((image) => {
+        image.removeEventListener('load', handleImageLoad);
+      });
+    };
+  }, []);
+  
   const getImageDataByCategory = () => {
     switch (selectedCategory) {
       case 'posters':
@@ -38,48 +65,35 @@ export const Gallery = ({ selectedCategory, imgFetched }: { selectedCategory: st
 
   return (
     <Container minH="100dvh" position="relative" maxW="100%" paddingLeft={{base: '20%', md:'10%', lg: '8%', xl: '6%'}} paddingTop="8rem">
-      <PhotoAlbum
-       photos={getSlides} 
-       layout="rows" 
-       targetRowHeight={500} 
-       onClick={({ index }) => setIndex(index)} 
-       />
-      <Lightbox
-        slides={getSlides}
-        open={index >= 0}
-        index={index}
-        close={() => setIndex(-1)}
-        // enable optional lightbox plugins
-        // plugins={[Slideshow, Zoom]}
-      />
-      
-      {/* <SimpleGrid
-        flexWrap="wrap"
-        display="grid"
-        minChildWidth="18.5em"
-        columns={[1, 2, 3]}
-        gap="1rem"
-        padding="0px"
-      >
-      {images.map((image) => (
-        <Image 
-          key={image.id} 
-          src={image.imageSrc} 
-          width={500} 
-          height={500} 
-          alt="test" 
-          onClick={(({index: current}) => setIndex(image.id))}
-          />
-        ))}
-        <Lightbox
-          index={index}
+      {!imagesLoaded && (
+        <Spinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="#F65252"
+          size="xl"
           position="absolute"
-          slides={getSlides}
-          open={index >= 0}
-          close={() => setIndex(-1)}
+          top="50%"
+          left="50%"
+          transform="translate(-50%, -50%)"
         />
-      </SimpleGrid>
-     */}
+      )}
+      {imagesLoaded && (
+        <>
+          <PhotoAlbum
+            photos={getSlides} 
+            layout="rows" 
+            targetRowHeight={500} 
+            onClick={({ index }) => setIndex(index)} 
+          />
+          <Lightbox
+            slides={getSlides}
+            open={index >= 0}
+            index={index}
+            close={() => setIndex(-1)}
+          />
+        </>
+      )}
     </Container>
   );
 };
